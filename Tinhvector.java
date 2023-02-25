b416c1d0d052974ae2ad11a2000496970d4e65ac
Sinh1050080155 <opolo4847@gmail.com>
2023-02-25 21:46:07 +0700

    lab3

--- Tinhvector.java
+++ Tinhvector.java
@@ -3,19 +3,23 @@ package BaiTapLab3;
 public class Tinhvector {
    
     public static void main(String[] args){
-        Vector t = new Vector(3,9,6);
-        Vector v = new Vector(3,9,6);
-        v.getx();
-        v.gety();
-        v.getz();
         
-        double x;
-        double y;
-        double z;
-        x = v.getx();
-        y = v.gety();
-        z = v.getz();
-        System.out.printf( "Tong 1 Vector= " + (x+ v.getx()) +", "+ (y + v.getx()) +", " + (z + v.getx()));
+        
+       Vector vt = new Vector();
+       vt.setx(7);
+       vt.sety(2);
+       vt.setz(3);
+       vt.seta(6);
+       vt.setb(7);
+       vt.setc(8);
+       vt.setk(6);
+        System.out.println(vt.toString());
+        System.out.println(vt.settong());
+        System.out.println(vt.sethieu());
+        System.out.println(vt.setnhanHSvt1());
+        System.out.println(vt.setnhanHSvt2());
+        System.out.println(vt.setnhan2vt());
+
        
     
 
--- Vector.java
+++ Vector.java
@@ -2,12 +2,15 @@ package BaiTapLab3;
 
 public class Vector {
 private double x, y, z;
+private double a,b,c;
+private double k;
 
 
-public Vector(double x, double y, double z ){
+public void Vector1(double x, double y, double z ){
     this.x = x;
     this.y = y;
     this.z = z; 
+    this.k = k;
 }
 
 public double getx(){
@@ -19,22 +22,91 @@ public double gety(){
 public double getz(){
     return z; 
 }
-  
+public double getk(){
+    return k; 
+}
+public double setx(double x){
+    return this.x = x;
+}
+public double sety(double y){
+    return this.y = y;
+}
+public double setz(double z){
+    return this.z = z;
+}
+public double setk(double k){
+    return this.k = k;
+}
 
-    public Vector cong(Vector v) {
-        return new Vector(x + v.getx(), y + v.getx(), z + v.getx());
-    }
-    public Vector tru(Vector v) {
-        return new Vector(x - v.getx(), y - v.getx(), z - v.getx());
-    }
-    public Vector nhan(double k) {
-        return new Vector(k * x, k * y, k * z);
-    }
-    public double nhanVH(Vector v) {
-        return x * v.getx() + y * v.getx() + z * v.getx();
-    }
+public void Vector2(double a, double b, double c ){
+    this.a = a;
+    this.b = b;
+    this.c = c; 
+}
+public double geta(){
+    return a; 
+}
+public double getb(){
+    return b; 
+}
+public double getc(){
+    return c; 
+}
+public double seta(double a){
+    return this.a = a;
+}
+public double setb(double b){
+    return this.b = b;
+}
+public double setc(double c){
+    return this.c = c;
+}
+public double settong(){
+    Double tong1 = this.x + this.a;
+    Double tong2 = this.y + this.b;
+    Double tong3 = this.z + this.c;
+    System.out.println("Tong 2 vector: " + "(x+a)= "+tong1 +" (y+b)= "+ tong2 +" (z+c)= "+ tong3);
+    return 0;
    
+}
+public double sethieu(){
+    Double hieu1 = this.a - this.x;
+    Double hieu2 = this.b - this.y;
+    Double hieu3 = this.c - this.z;
+    System.out.println("Hieu 2 vector: " + "(a-x)= "+hieu1 +" (b-y)= "+ hieu2 +" (c-z)= "+ hieu3);
+    return 0;
    
+}
+public double setnhanHSvt1(){
+    Double nhanhs1 = this.x * this.k;
+    Double nhanhs2 = this.y * this.k;
+    Double nhanhs3 = this.z * this.k;
+    System.out.println("Vector1 nhan voi hang so: " + "(x*k)= "+nhanhs1 +" (y*k)= "+ nhanhs2 +" (z*k)= "+ nhanhs3);
+    return 0;
+   
+}
+public double setnhanHSvt2(){
+    Double nhanhs1 = this.a * this.k;
+    Double nhanhs2 = this.b * this.k;
+    Double nhanhs3 = this.c * this.k;
+    System.out.println("Vector1 nhan voi hang so: " + "(a*k)= "+nhanhs1 +" (b*k)= "+ nhanhs2 +" (c*k)= "+ nhanhs3);
+    return 0;
+   
+}
+public double setnhan2vt(){
+    Double nhan2vt1 = this.a * this.x;
+    Double nhan2vt2 = this.b * this.y;
+    Double nhan2vt3 = this.c * this.z;
+    System.out.println("Nhan 2 vector: " + "(a*x)= "+nhan2vt1 +" (b*y)= "+ nhan2vt2 +" (c*z)= "+ nhan2vt3);
+    return 0;
+   
+}
+public String toString(){
+    return "x = " + x + " y = " + y + " z = " + z + " a = " + a + " b = " + b + " c = " + c;
+    
+}
+
+
 } 
     
     
